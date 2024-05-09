8bafbeded717c0c5cef946b610aeba22900d90ac
ilya_popov <il_pop@mail.ru>
2024-05-09 19:53:58 +0300

    creater superuser, model DateConverter

--- django_app/converter/models.py
+++ django_app/converter/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+
+class DateConverter(models.Model):
+    date_json = models.TextField(help_text='Строка с данными из запроса к API в виде строки json',
+                                 verbose_name="Данные для конвертации")
